f0bb390e19adaf62c7e524c5395e5ab692fa1289
takahiro mitsuoka <tmitsuoka0423@gmail.com>
2021-01-23 15:20:13 +0900

    remove server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const port = process.env.PORT || 3000;
-
-const app = express();
-app.post('/', (req, res) => {
-  console.log(req);
-  res.send("received!");
-});
-app.listen(port, () => console.log(`http://localhost:${port}`))
